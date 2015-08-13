234fe143fd9c3027369c365ad31d062d168c7ed8
Jocelyn Mallon <jocelyn.e.mallon@gmail.com>
2015-08-12 18:49:56 -0700

    Initial Commit
    Initial Commit

---
+++ init.zsh
@@ -0,0 +1,11 @@
+# ------------------------------------------------------------------------------
+#          FILE:  ttv.zsh
+#   DESCRIPTION:  twitch.tv/livestreamer plugin file.
+#        AUTHOR:  Jocelyn Mallon
+#       VERSION:  0.0.2
+# ------------------------------------------------------------------------------
+
+for file in $HOME/.zprezto/modules/twitchtv/functions/*.zsh; do
+  source $file
+done
+
